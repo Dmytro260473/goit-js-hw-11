9a36effcee3b05bea99efbff8714f5820fadaf06
Dmitry Stephanik <dmitrystephanik@MacBook-Pro-Dmitry.local>
2025-07-20 22:12:23 +0300

    hv

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-js-hw-11/', // ДОДАЙ ЦЕ
+
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
